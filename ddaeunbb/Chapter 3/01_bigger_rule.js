9144ff731c43189204a17c4dd041f54dd242e1be
ddaeunbb <ddaeunbb@gmail.com>
2023-04-19 22:44:00 +0900

    [그리디] : 실전문제1 풀이

--- ddaeunbb/Chapter 3/01_bigger_rule.js
+++ ddaeunbb/Chapter 3/01_bigger_rule.js
@@ -1,22 +1,38 @@
+// function biggerrule(m, k, arr){
+//   arr.sort((a,b)=> b-a);
+//   const first = arr[0];
+//   const second = arr[1];
+//   let count = 0;
+//   let previous = k;
+
+//   while( m > 0 ){
+//     if (previous > 0 ){
+//       count+=first;
+//       previous--;
+//     }
+//     else {
+//       count+=second
+//       previous = k;
+//     }
+//     m--;
+//   }
+//   return count;
+// }
+
 function biggerrule(m, k, arr){
   arr.sort((a,b)=> b-a);
   const first = arr[0];
   const second = arr[1];
-  let count = 0;
-  let previous = k;
+  const total = first * k + second;
+  let result = 0;
 
-  while( m > 0 ){
-    if (previous > 0 ){
-      count+=first;
-      previous--;
-    }
-    else {
-      count+=second
-      previous = k;
-    }
-    m--;
-  }
-  return count;
+  let totalCount = parseInt(m / (k+1));
+  let firstCount = m % (k+1);
+
+  result += totalCount * total;
+  result += firstCount * first;
+
+  return result;
 }
 
 console.log(biggerrule(8, 3, [2,4,5,4,6]))
