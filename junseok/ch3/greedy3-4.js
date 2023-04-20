7d62dea048c2fb124b729a71fa69413713ad3faa
JUNSEOK <kd02109@gmail.com>
2023-04-20 18:49:37 +0900

    [code]: 1이 될때까지 다른 방법 작성

--- junseok/ch3/greedy3-4.js
+++ junseok/ch3/greedy3-4.js
@@ -12,3 +12,22 @@ function solution(N, K) {
   return count;
 }
 console.log(solution(25, 3));
+
+//방법2
+// 몫과 나머지를 구해서 1을 빼는 과정을 한 번에 처리한다
+function solution2(N, K) {
+  let num = N;
+  let count = 0;
+  while (num > 1) {
+    let target = Math.floor(num / K) * K;
+    count += num % K;
+    num = target;
+    if (num < K) {
+      break;
+    }
+    num /= K;
+    count++;
+  }
+  return count - 1;
+}
+console.log(solution2(25, 3));
