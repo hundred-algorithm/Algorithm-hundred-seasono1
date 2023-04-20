7a953aaea9975f841fb6f12537f8e22b0229a49f
ddaeunbb <ddaeunbb@gmail.com>
2023-04-20 09:32:28 +0900

    [그리디]: 실전문제3 풀이

--- ddaeunbb/Chapter 3/03_until_one.js
+++ ddaeunbb/Chapter 3/03_until_one.js
@@ -13,9 +13,14 @@ function untilOne(N, K){
   let count = 0;
 
   while ( N > 1){
-    if ( N % K === 0) N = N / K;
-    else N--;
-    count++;
+    if ( N % K === 0) {
+      N = N / K;
+      count++;
+    } else {
+      let minus = K > N ?  N % K - 1 : N % K;
+      N -= minus;
+      count += minus;
+    };
   }
   return count;
 }
