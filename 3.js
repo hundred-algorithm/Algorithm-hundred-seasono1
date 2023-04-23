93a3a1f5a173511a4becdb296c569bdd660f2bef
j_h___ <85489123+jihoon2819@users.noreply.github.com>
2023-04-24 00:09:57 +0900

    Delete 3.js

--- 3.js
+++
@@ -1,7 +0,0 @@
-function 문자열남기기(numbers){
-     numbers=numbers.split(/0+/);
-    const zero = numbers.filter((value)=>value==='').length
-    const one = numbers.length-zero
-    return zero>one?one:zero
-}
-console.log(문자열남기기("0001100"));
